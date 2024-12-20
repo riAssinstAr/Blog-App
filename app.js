b2bc2c81cde30d989632d5f71d748e0f1a0c08bd
riAs-g <abhi2308204@gmail.com>
2024-12-20 20:06:29 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://<username>:<password>@<database-uri>";
+
+mongoose.connect(dbURI)
+  .then(() => app.listen(3000))
+  .catch(err => {
+    if (err)
+      console.log(err)
+    else
+      console.log('Server is running')});
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
